337dce1e34d4902925b3fb38d5122cc61cab66b0
Ysiad Ferreiras <ysiadf@gmail.com>
2012-02-05 15:34:42 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Industryparty::Application.config.secret_token = 'f827725934f892a5bde553cb511adc40a3ab06dcdaa0574a4678d959f54ca849f411ae846d1ea7e296d783ab28eb2e2804f9f3ae3c9fa31d9d750df2bed4c03e'
